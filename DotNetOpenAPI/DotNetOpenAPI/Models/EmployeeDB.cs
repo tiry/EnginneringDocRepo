cc0f6735bdcbc2bfdebb6c45dc9962278695d893
Kaushik Natua <Kaushik.Natua@hyland.com>
2024-12-17 16:52:42 +0530

    First Push

---
+++ DotNetOpenAPI/DotNetOpenAPI/Models/EmployeeDB.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetOpenAPI.Models
+{
+    public class EmployeeDB
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+    }
+}
