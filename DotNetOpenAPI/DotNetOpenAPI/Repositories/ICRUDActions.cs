937f7abe7d906ccb138a8896389ea07fb6094b26
Kaushik Natua <Kaushik.Natua@hyland.com>
2025-01-02 19:11:44 +0530

    Refactored code

---
+++ DotNetOpenAPI/DotNetOpenAPI/Repositories/ICRUDActions.cs
@@ -0,0 +1,9 @@
+﻿using DotNetOpenAPI.Models;
+namespace DotNetOpenAPI.Repositories
+{
+    public interface ICRUDActions
+    {
+        public List<EmployeeDB> GetEmployees();
+        public EmployeeDB? GetEmployee(int id);
+    }
+}
