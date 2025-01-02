dca4fbb6334bdebd680e93401c4e715aece2e427
Kaushik Natua <Kaushik.Natua@hyland.com>
2025-01-02 19:59:06 +0530

    refactored .net open api codes

---
+++ DotNetOpenAPI/DotNetOpenAPI/Program.cs
@@ -0,0 +1,73 @@
+using DotNetOpenAPI.Repositories;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<ICRUDActions,CRUDActions>();
+
+
+
+//enable cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+//Swagger Documentation Section
+var info = new OpenApiInfo()
+{
+    Title = "DotNet CRUD WebAPI With Open API Documentation",
+    Version = "v1",
+    Description = "This is a sample API created using .NET Core to test documentation process using OpenAPI",
+    Contact = new OpenApiContact()
+    {
+        Name = "Kaushik Natua",
+        Email = "kaushik.natua@hyland.com",
+    }
+};
+
+// Set the comments path for the Swagger JSON and UI.
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", info);
+
+    // Set the comments path for the Swagger JSON and UI in json format for swaager.josn file
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);    
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(u =>
+    {
+        u.RouteTemplate = "swagger/{documentName}/swagger.json";
+    });
+
+    app.UseSwaggerUI(c =>
+    {
+        c.RoutePrefix = "swagger";
+        c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "DotNet CRUD API or Version");
+    });
+}
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+
+app.Run();
